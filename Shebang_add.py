ed5b06712054c6db3e6386f0653545b457aaa0bf
Jakob Frey <jakob.frey@tutanota.com>
2023-11-04 18:31:49 +0100

    included config.json in Shebang_add.py

--- Shebang_add.py
+++ Shebang_add.py
@@ -2,27 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from argparse import ArgumentParser
+import json
 
-parser = ArgumentParser(description = "Ein Programm um schnell und einafch Shebangs in Skripten zu ergaenzen")
-parser.add_argument("file", metavar="Datei", type=str, help="Dateiname des Skripts")
-parser.add_argument("interpreter", metavar="Interpreter", type=str, help="Name des Interpreters")
-parser.add_argument("-e", "--executable", default = False, help="Option zum direkten Markieren als ausfuehrbare Datei")
-parser.add_argument("-v", "--version", action="version", version="0.8", help="Display the current version")
+with open("config.json") as conFile:
+    data = json.load(conFile)
+
+Config = data[0]
+Interpreters = data[1]
+Languages = data[2]
+
+outputLanguage = Config["defaultLanguage"]
+sentences = Languages[outputLanguage]
+print(sentences)
+
+parser = ArgumentParser(description = sentences[0])
+parser.add_argument("file", metavar=sentences[1], type=str, help=sentences[2])
+parser.add_argument("interpreter", metavar="Interpreter", type=str, help=sentences[3])
+parser.add_argument("-e", "--executable", default = False, help=sentences[4])
+parser.add_argument("-v", "--version", action="version", version="0.90", help=sentences[5])
 args = parser.parse_args()
-print(args.executable)
-
-Interpreters = {
-    "Python": "#!/usr/bin/env python3",
-    "Bash": "#!/usr/bin/bash",
-    "Zsh": "#!/usr/bin/zsh",
-    "Shell": "#!/usr/bin/sh"
-    }
     
 Ip = args.interpreter
 if Ip in Interpreters:
     Shebang = Interpreters[Ip]
 else:
-    Shebang = Ip
+    Shebang = ''.join(["#!", Ip])
 
 try:
     with open(args.file, "r+") as script:
@@ -33,7 +37,7 @@ try:
             del(content[0])
         script.write(Shebang+ "\n"+ "".join(content))
 except FileNotFoundError:
-    print("Die angegebene Datei existiert nicht.")
+    print(sentences[6])
 
 if args.executable == "True":
     from os import system
