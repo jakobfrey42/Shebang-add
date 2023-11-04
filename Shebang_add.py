46de71de35947dc1bab4aba4e8adc188d9378b6c
Jakob Frey <jakob.frey@tutanota.com>
2023-11-04 13:19:48 +0100

    added argument -v/--version

--- Shebang_add.py
+++ Shebang_add.py
@@ -7,6 +7,7 @@ parser = ArgumentParser(description = "Ein Programm um schnell und einafch Sheba
 parser.add_argument("file", metavar="Datei", type=str, help="Dateiname des Skripts")
 parser.add_argument("interpreter", metavar="Interpreter", type=str, help="Name des Interpreters")
 parser.add_argument("-e", "--executable", default = False, help="Option zum direkten Markieren als ausfuehrbare Datei")
+parser.add_argument("-v", "--version", action="version", version="0.8", help="Display the current version")
 args = parser.parse_args()
 print(args.executable)
 
@@ -18,15 +19,22 @@ Interpreters = {
     }
     
 Ip = args.interpreter
-Shebang = Interpreters[Ip]
 if Ip in Interpreters:
-    try:
-        with open(args.file, "r+") as script:
-            content = script.readlines()
-            script.seek(0,0)
-            firstLineLst = list(content[0])
-            if firstLineLst[0] == "#" and firstLineLst[1] == "!":
-                del(content[0])
-            script.write(Shebang+ "\n"+ "".join(content))
-    except FileNotFoundError:
-        print("Die angegebene Datei existiert nicht.")
+    Shebang = Interpreters[Ip]
+else:
+    Shebang = Ip
+
+try:
+    with open(args.file, "r+") as script:
+        content = script.readlines()
+        script.seek(0,0)
+        firstLineLst = list(content[0])
+        if firstLineLst[0] == "#" and firstLineLst[1] == "!":
+            del(content[0])
+        script.write(Shebang+ "\n"+ "".join(content))
+except FileNotFoundError:
+    print("Die angegebene Datei existiert nicht.")
+
+if args.executable == "True":
+    from os import system
+    system("chmod +x "+ args.file)
